43328b4641f872c5d0810e97e56bc10ff0ade843
mike.duren <mike@cybermoxie.com>
2020-12-10 20:16:38 -0500

    Added files.

---
+++ BackupRestoreRFS.ps1
@@ -0,0 +1,146 @@
+﻿param (
+  $BackupDestination = "C:\test", 
+  $LogFile = "copiedFiles.csv", 
+  $KmDataSource = 'C:\ProgramData\nCipher\Key Management Data',
+  [switch]$Restore = $false,
+  [switch]$IncludeADCS = $false
+)
+
+
+####################################################################################
+#
+#  FailedBanner
+#    
+#
+####################################################################################
+function FailedBanner 
+{
+    param ($message)
+
+    Write-Host "**********************************************************"
+    Write-Host "*                                                        *"
+    Write-Host "*                         FAILED                         *"
+    Write-Host "*                                                        *"
+    Write-Host "**********************************************************"
+    Write-Host 
+    Write-Host $message
+    Write-Host 
+}
+
+####################################################################################
+#
+#  PassedBanner
+#    
+#
+####################################################################################
+function PassedBanner 
+{
+    param ($message)
+
+    Write-Host "**********************************************************"
+    Write-Host "*                                                        *"
+    Write-Host "*                       Complete                         *"
+    Write-Host "*                                                        *"
+    Write-Host "**********************************************************"
+    Write-Host 
+}
+
+if (Test-Path $LogFile)
+{
+    Remove-Item -Path $LogFile
+}
+
+# the script name is used in the target folder name
+$scriptName = [io.path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+#create a folder in the target folder
+$BackupFolder = Join-Path -Path $BackupDestination -ChildPath $currentDate.ToString("dd-MM-yyyy")
+$BackupFolder = $BackupFolder + "-" + $scriptName
+
+if (Test-Path $BackupFolder)
+{
+    #exists, what do we do?
+}
+else
+{
+    New-Item -Path $BackupFolder -Type Directory
+}
+
+$kmDataBackupFolder = Join-Path -Path $BackupFolder -ChildPath "kmdata-local"
+
+try
+{         
+    if ($Restore)
+    {
+        .\CopyAndRecord.ps1 -CopyFrom $kmDataBackupFolder -CopyTo $KmDataSource -LogFile $LogFile -Recurse -ErrorAction stop
+    }
+    else
+    {
+        .\CopyAndRecord.ps1 -CopyFrom $KmDataSource -CopyTo $kmDataBackupFolder -LogFile $LogFile -Recurse -ErrorAction stop
+    }
+}
+catch
+{ 
+    FailedBanner -message "Failed to copy HSM key management data"
+
+    Write-Host $_
+    exit
+}
+
+
+if ($IncludeADCS)
+{
+    try
+    {         
+        $dbBackupFolder = Join-Path -Path $BackupFolder -ChildPath "DBbackup"
+
+        # copy the cert files
+        .\CopyAndRecord.ps1 -CopyFrom $env:windir\system32\certsrv\certenroll -CopyTo $dbBackupFolder -Pattern "*.crt" -LogFile $LogFile
+        .\CopyAndRecord.ps1 -CopyFrom $env:windir\system32\certsrv\certenroll -CopyTo $dbBackupFolder -Pattern "*.crl" -LogFile $LogFile
+        .\CopyAndRecord.ps1 -CopyFrom $env:windir -CopyTo $dbBackupFolder -Pattern "CAPolicy.inf" -LogFile $LogFile
+
+        # backup the cert database
+        $certutilProgram = Join-Path -Path $env:windir -ChildPath "\system32\certutil.exe"
+        $arguments = "-backupDB `"$dbBackupFolder`""
+        $p = Start-Process $certutilProgram -ArgumentList $arguments -wait -NoNewWindow -passthru  -RedirectStandardOutput temp.fil -RedirectStandardError errors.txt
+
+        if ($p.ExitCode -ne 0)
+        {
+            Write-Host "certutil command failed FAILED"
+            exit
+        }
+
+
+        # export the registry configuation data
+        $regProgram = Join-Path -Path $env:windir -ChildPath "\system32\reg.exe"
+        $arguments = "export HKLM\system\currentcontrolset\services\certsvc\configuration caregistry.regr"
+        $p = Start-Process $regProgram -ArgumentList $arguments -wait -NoNewWindow -passthru  -RedirectStandardOutput temp.fil -RedirectStandardError errors.txt
+
+        $p.HasExited
+        if ($p.ExitCode -ne 0)
+        {
+            Write-Host "FAILED"
+            exit
+        }    
+    }
+    catch
+    { 
+        Write-Host "FAILED"
+        Write-Host $_
+    }
+}
+
+
+# success if we reach this point
+PassedBanner
+$copiedFiles = Import-Csv -Path $LogFile 
+
+Write-Host $copiedFiles.Count files copied.
+
+$copiedFiles | Format-Table -AutoSize -Property Filename, Digest, CopiedFrom
+
+Write-Host "File details copied to" $LogFile
+
+
+
+
---
+++ CopyAndRecord.ps1
@@ -0,0 +1,120 @@
+﻿param (
+  [Parameter(Mandatory=$true)]$CopyTo = "", 
+  $LogFile = "", 
+  $Pattern = "*",
+  [switch]$Recurse = $false,
+  [Parameter(Mandatory=$true)]$CopyFrom = "",
+  [switch]$KmData = $false,
+  [switch]$CertDb = $false
+)
+
+if ($LogFile.Length -gt 0)
+{
+    $currentDate = Get-Date
+    $filesTransferred = New-Object System.Data.DataTable
+    $filesTransferred.Columns.Add("Filename", "System.String") | Out-Null
+    $filesTransferred.Columns.Add("CopiedFrom", "System.String") | Out-Null
+    $filesTransferred.Columns.Add("CopiedTo", "System.String") | Out-Null
+    $filesTransferred.Columns.Add("Digest", "System.String") | Out-Null
+    $filesTransferred.Columns.Add("Date", "System.String") | Out-Null
+}
+
+
+####################################################################################
+#
+#  CopyAndRecord
+#    
+#
+####################################################################################
+function CopyAndRecord 
+{
+    param($parentFolder, $destinationParentFolder)
+
+    # start with the files 
+    $childItems = Get-ChildItem -Force -Path $parentFolder -File -Filter $Pattern
+    if ($childItems.Count -gt 0)
+    {
+        foreach($item in $childItems)
+        {
+            $destinationItem = Join-Path -Path $destinationParentFolder -ChildPath $item.Name
+
+            try 
+            {
+                $itemHash = Get-FileHash -Path $item.FullName -Algorithm SHA256 -errorAction stop
+                Copy-Item -Force -Path $item.FullName -Destination $destinationItem -errorAction stop
+
+                # record the detaisl of the file that was just copied
+                $nRow = $filesTransferred.NewRow()
+                $nRow.Filename = $item.Name
+                $nRow.CopiedFrom = Split-Path -Path $item.FullName -Parent
+                $nRow.CopiedTo = $destinationItem
+                $nRow.Digest = $itemHash.Hash
+                $nRow.Date = $currentDate.ToLocalTime()
+                $filesTransferred.Rows.Add($nRow)
+            }
+            catch 
+            {
+                # tell the caller there was an error
+                throw $_
+            }
+        }
+    }
+
+    if ($Recurse)
+    {
+        # recusively create and then copy the folders
+        $childItems = Get-ChildItem -Path $parentFolder -Directory
+        if ($childItems.Count -gt 0)
+        {
+            foreach($item in $childItems)
+            {
+                $destinationItem = Join-Path -Path $destinationParentFolder -ChildPath $item.Name
+                try 
+                {
+                    if ((Test-Path -Path $destinationItem) -eq $false) 
+                    { 
+                        New-Item -Path $destinationItem -Type Directory
+                    }
+                }
+                catch 
+                {
+                    # tell the caller there was an error
+                    throw $_
+                }
+
+                try
+                {
+                    CopyAndRecord -parentFolder $item.FullName -destinationParentFolder $destinationItem
+                }
+                catch
+                {
+                    # this makes sure the recursion unwraps so the original caller gets the error
+                    throw $_
+                }
+            }
+        }
+    }
+}
+
+if (Test-Path $CopyTo)
+{
+    #exists, what do we do?
+}
+else
+{
+    New-Item -Path $CopyTo -Type Directory
+}
+
+try
+{    
+    CopyAndRecord -parentFolder $CopyFrom $CopyTo
+
+    if ($LogFile.Length -gt 0)
+    {
+        $filesTransferred | Export-Csv $LogFile -notypeinformation -Append
+    }
+}
+catch
+{ 
+    throw $_
+}
